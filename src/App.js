226f8875e4a525c54765f45aeb1c594776b016c0
Angelo Fernandes Machado Fonseca <angelofmfonseca@gmail.com>
2020-05-22 23:11:25 -0300

    axios installation and general request

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { Component } from "react";
+import axios from "axios";
 
-function App() {
-  return <div className="App">Hey</div>;
+class App extends Component {
+  componentDidMount() {
+    const url = "https://covid19.mathdro.id/api";
+    axios
+      .get(url)
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+      .finally(() => {
+        console.log("fatching data completed");
+      });
+  }
+
+  render() {
+    return <div className="App"></div>;
+  }
 }
 
 export default App;
