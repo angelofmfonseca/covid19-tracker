fef9fbf237b553aa50f2e136f4f8d44f63783776
Angelo Machado Fonseca <angelofmfonseca@gmail.com>
2020-05-23 01:46:59 -0300

    styled-components and base wireframe

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import styled from "styled-components";
 
 import { getData } from "./services";
 
@@ -27,11 +28,65 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <span>{this.state.data.confirmed}</span>
-      </div>
+      <Main className="App">
+        <Title>
+          COVID-19
+          <SubTitle>Real Time Tracker</SubTitle>
+        </Title>
+        <DataWrapper>
+          <GeneralInfo>
+            <SelectCountry>World</SelectCountry>
+            <LastUpdate>{this.state.data.lastUpdate}</LastUpdate>
+          </GeneralInfo>
+          <SpecificInfo>
+            <span>{this.state.data.confirmed}</span>
+            <span>{this.state.data.recovered}</span>
+            <span>{this.state.data.deaths}</span>
+          </SpecificInfo>
+        </DataWrapper>
+      </Main>
     );
   }
 }
+
+const Main = styled.main`
+  text-align: center;
+  margin: 20px;
+`;
+
+const Title = styled.span`
+  font-size: 3rem;
+`;
+
+const SubTitle = styled.span`
+  font-size: 1.5rem;
+  display: block;
+`;
+
+const DataWrapper = styled.div`
+  width: 70%;
+  margin: 50px auto;
+  border: #ccc solid 1px;
+`;
+
+const GeneralInfo = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 70%;
+  margin: 0 auto;
+`;
+
+const SelectCountry = styled.span`
+  font-size: 1rem;
+`;
+
+const LastUpdate = styled.span`
+  font-size: 1rem;
+`;
+
+const SpecificInfo = styled.div`
+  display: flex;
+  justify-content: space-around;
+`;
 
 export default App;
