920ebc5faf6155dbc2c3badab3120a6ef023ad1e
Angelo Fernandes Machado Fonseca <angelofmfonseca@gmail.com>
2020-05-22 22:51:04 -0300

    correcting import error

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 
-import "./App.css";
+function App() {
+  return <div className="App">Hey</div>;
+}
 
-export const App = () => {
-  return <div className="App"></div>;
-};
+export default App;
