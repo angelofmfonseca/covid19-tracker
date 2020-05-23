4e82ca80c13f258981ce364ff47f104594896c5f
Angelo Machado Fonseca <angelofmfonseca@gmail.com>
2020-05-23 14:01:33 -0300

    react-moment + countup + cards working

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import styled from "styled-components";
+import Moment from "react-moment";
+import CountUp from "react-countup";
 
 import { getData } from "./services";
 
@@ -27,21 +29,40 @@ class App extends Component {
   }
 
   render() {
+    const { confirmed, recovered, deaths, lastUpdate } = this.state.data;
     return (
-      <Main className="App">
+      <Main>
         <Title>
           COVID-19
           <SubTitle>Real Time Tracker</SubTitle>
         </Title>
         <DataWrapper>
           <GeneralInfo>
-            <SelectCountry>World</SelectCountry>
-            <LastUpdate>{this.state.data.lastUpdate}</LastUpdate>
+            <TopTitle>World</TopTitle>
+            <TopTitle>
+              <LastUpdateTitle> Last Update:</LastUpdateTitle>
+              <Moment data={lastUpdate} format="MM/DD/YYYY - HH:mm" />
+            </TopTitle>
           </GeneralInfo>
           <SpecificInfo>
-            <span>{this.state.data.confirmed}</span>
-            <span>{this.state.data.recovered}</span>
-            <span>{this.state.data.deaths}</span>
+            <DataCard>
+              <CardTitle>Confirmed:</CardTitle>
+              <CardData>
+                <CountUp start={0} end={confirmed} separator="." />
+              </CardData>
+            </DataCard>
+            <DataCard>
+              <CardTitle>Recovered:</CardTitle>
+              <CardData>
+                <CountUp start={0} end={recovered} separator="." />
+              </CardData>
+            </DataCard>
+            <DataCard>
+              <CardTitle>Deaths:</CardTitle>
+              <CardData>
+                <CountUp start={0} end={deaths} separator="." />
+              </CardData>
+            </DataCard>
           </SpecificInfo>
         </DataWrapper>
       </Main>
@@ -66,7 +87,9 @@ const SubTitle = styled.span`
 const DataWrapper = styled.div`
   width: 70%;
   margin: 50px auto;
-  border: #ccc solid 1px;
+  padding: 30px;
+  border-radius: 5px;
+  box-shadow: 0.5px 0.5px 5px 0.5px #ddd;
 `;
 
 const GeneralInfo = styled.div`
@@ -74,19 +97,37 @@ const GeneralInfo = styled.div`
   justify-content: space-between;
   width: 70%;
   margin: 0 auto;
+  padding-bottom: 30px;
 `;
 
-const SelectCountry = styled.span`
-  font-size: 1rem;
+const TopTitle = styled.span`
+  font-size: 1.3rem;
 `;
 
-const LastUpdate = styled.span`
-  font-size: 1rem;
+const LastUpdateTitle = styled.span`
+  display: block;
+  text-align: end;
 `;
 
 const SpecificInfo = styled.div`
   display: flex;
   justify-content: space-around;
+`;
+
+const DataCard = styled.div`
+  padding: 20px 50px;
+  border-radius: 5px;
+  box-shadow: 0.5px 0.5px 5px 0.5px #ddd;
+`;
+
+const CardTitle = styled.span`
+  display: block;
+  padding-bottom: 5px;
+  font-size: 1.3rem;
+`;
+
+const CardData = styled.span`
+  font-size: 1.2rem;
 `;
 
 export default App;
