ca462b22422a516b8fe94f081a503f7115c8a191
Angelo Fernandes Machado Fonseca <angelofmfonseca@gmail.com>
2020-06-27 14:14:32 -0300

    refactor main

--- src/Main/index.js
+++ src/Main/index.js
@@ -1,11 +1,11 @@
 import React, { Component } from "react";
-import styled from "styled-components";
 
 import { getData } from "../Services/getData";
 import { getCountry } from "../Services/getCountry";
 import Cards from "../Cards";
 import Select from "../Select";
 import LastUpdate from "../LastUpdate";
+import { Main, Title, SubTitle, DataWrapper, GeneralInfo } from "./styles";
 
 class App extends Component {
   state = {
@@ -53,36 +53,5 @@ class App extends Component {
     );
   }
 }
-
-const Main = styled.main`
-  text-align: center;
-  margin: 20px;
-`;
-
-const Title = styled.span`
-  font-size: 3rem;
-`;
-
-const SubTitle = styled.span`
-  font-size: 1.5rem;
-  display: block;
-`;
-
-const DataWrapper = styled.div`
-  width: 70%;
-  margin: 50px auto;
-  padding: 30px;
-  border-radius: 5px;
-  box-shadow: 0.5px 0.5px 5px 0.5px #ddd;
-  background-color: #fff;
-`;
-
-const GeneralInfo = styled.div`
-  display: flex;
-  justify-content: space-between;
-  width: 80%;
-  margin: 0 auto;
-  padding-bottom: 30px;
-`;
 
 export default App;
---
+++ src/Main/styles.js
@@ -0,0 +1,32 @@
+import styled from "styled-components";
+
+export const Main = styled.main`
+  text-align: center;
+  margin: 20px;
+`;
+
+export const Title = styled.span`
+  font-size: 3rem;
+`;
+
+export const SubTitle = styled.span`
+  font-size: 1.5rem;
+  display: block;
+`;
+
+export const DataWrapper = styled.div`
+  width: 70%;
+  margin: 50px auto;
+  padding: 30px;
+  border-radius: 5px;
+  box-shadow: 0.5px 0.5px 5px 0.5px #ddd;
+  background-color: #fff;
+`;
+
+export const GeneralInfo = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 80%;
+  margin: 0 auto;
+  padding-bottom: 30px;
+`;
